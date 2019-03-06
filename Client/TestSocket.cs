47f6852323212632d18516bfb060d91db3dbaa48
DirtyCeiling <44331165+DirtyCeiling@users.noreply.github.com>
2019-03-06 09:47:31 +0800

    Add files via upload

---
+++ Client/TestSocket.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using Net;
+
+public class TestSocket : MonoBehaviour
+{
+    public ClientSocket mSocket = new ClientSocket();
+    // Use this for initialization
+    void Start()
+    {
+       
+        mSocket.ConnectServer("127.0.0.3", 8088);
+        mSocket.SendMessage("你好服务器！");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
